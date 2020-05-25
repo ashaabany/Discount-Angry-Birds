2b2adcac29ed4749e533eeec98a914b359fd85fe
ashaabany <48104665+ashaabany@users.noreply.github.com>
2020-05-25 09:43:16 -0700

    Add files via upload

---
+++ Assets/Bird.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+   private Vector2 _initialPosition;
+   private bool _birdWasLaunced;
+   private float _timeSittingAround;
+
+   [SerializeField] private float _launchPower;
+
+
+   private void Awake()
+   {
+      _initialPosition = transform.position;
+   
+
+   }
+
+   private void Update() 
+   {
+      
+      GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+      GetComponent<LineRenderer>().SetPosition(0, transform.position);
+
+
+
+      if (_birdWasLaunced && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1) {
+         _timeSittingAround += Time.deltaTime;
+      }
+
+
+      if (transform.position.y >= 25 || transform.position.x <= -30 || transform.position.x >= 20 || _timeSittingAround > 1.5) {
+         string currentSceneName = SceneManager.GetActiveScene().name;
+         SceneManager.LoadScene(currentSceneName);
+      }
+
+   }
+   
+   public void onMouseDown() 
+    {
+       GetComponent<LineRenderer>().enabled = true;
+    }
+   
+    
+    public void OnMouseUp() 
+    {
+       
+      Vector2 newPosition = new Vector2(transform.position.x, transform.position.y);
+      Vector2 direction = _initialPosition - newPosition; 
+
+      GetComponent<Rigidbody2D>().AddForce(direction* _launchPower);
+
+      GetComponent<Rigidbody2D>().gravityScale = 1;
+
+      _birdWasLaunced = true;
+      GetComponent<LineRenderer>().enabled = false;
+
+    }
+
+
+    public void OnMouseDrag()
+    {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = newPosition;
+
+    }
+   
+
+}
---
+++ Assets/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+
+        bool didHitBird = collision.collider.GetComponent<Bird>() != null;
+
+        if (didHitBird) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Monster monster = collision.collider.GetComponent<Monster>();
+        if (monster != null) {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y <-0.5) {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+}
